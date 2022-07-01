f4511485df6ffcfda7028f66ad300e99d1160d70
SugarSBN <15952265117@126.com>
2022-07-01 16:37:58 +0800

    change to Generate a

--- app/CodeGen.hs
+++ app/CodeGen.hs
@@ -12,11 +12,14 @@ data Assembly = Assembly {
     fieldL    :: Integer,
     fieldA    :: Integer,
     callEntry :: String
-}   
+}  | ProcedureEntry String
+
 instance Show Assembly where
     show (Assembly opt l a ent) = (show opt) ++ " " ++ (show l) ++ " " ++ (show a) ++ " " ++ ent
 
-genFactor :: Factor -> Integer -> SymTable -> Maybe [Assembly]
+type Generate a = a -> Integer -> SymTable -> Maybe [Assembly]
+
+genFactor :: Generate Factor
 genFactor (FactorId id) lev symTable = case ent of
     Nothing -> Nothing
     Just e  -> case kind e of 
@@ -27,7 +30,7 @@ genFactor (FactorId id) lev symTable = case ent of
 genFactor (FactorInt x) lev symTable = Just [Assembly LIT 0 x ""]
 genFactor (FactorExpr x) lev symTable = genExpr x lev symTable
 
-genItem :: Item -> Integer -> SymTable -> Maybe [Assembly]
+genItem :: Generate Item
 genItem (Item factor rest) lev symTable = case reverse rest of
     []       -> genFactor factor lev symTable
     (x : vs) -> (++) <$> (
@@ -38,7 +41,7 @@ genItem (Item factor rest) lev symTable = case reverse rest of
                         '*' -> Just [Assembly OPR 0 4 ""]
                         '/' -> Just [Assembly OPR 0 5 ""]
 
-genExpr :: Expr -> Integer -> SymTable -> Maybe [Assembly]
+genExpr :: Generate Expr
 genExpr (Expr mark item rest) lev symTable = case reverse rest of
     []       -> (++) <$> genItem item lev symTable <*> Just (if (isNeg mark) then [Assembly OPR 0 1 ""] else [])
     (x : vs) -> (++) <$> (
@@ -49,14 +52,14 @@ genExpr (Expr mark item rest) lev symTable = case reverse rest of
                         '+' -> Just [Assembly OPR 0 2 ""]
                         '-' -> Just [Assembly OPR 0 3 ""]
                          
-genAssign :: Assign -> Integer -> SymTable -> Maybe [Assembly]
+genAssign :: Generate Assign
 genAssign (Assign id expr) lev symTable = case ent of
         Nothing -> Nothing
         Just e  -> (++) <$> genExpr expr lev symTable <*> Just [Assembly STO (lev - (level e)) (addr e) ""]
     where
         ent = lookUp symTable id lev
 
-genCondition :: Condition -> Integer -> SymTable -> Maybe [Assembly]
+genCondition :: Generate Condition
 genCondition (Odd expr) lev symTable = (++) <$> genExpr expr lev symTable <*> Just [Assembly OPR 0 6 ""]
 genCondition (Condition e1 opt e2) lev symTable = 
     (++) <$> (
@@ -69,24 +72,25 @@ genCondition (Condition e1 opt e2) lev symTable =
         ">"  -> Just [Assembly OPR 0 12 ""]
         "<=" -> Just [Assembly OPR 0 13 ""]
 
-genProcedureCall :: ProcedureCall -> Integer -> SymTable -> Maybe [Assembly]
+genProcedureCall :: Generate ProcedureCall
 genProcedureCall (ProcedureCall f) lev symTable = Just [Assembly CAL 0 0 f]
 
-genComRead :: ComRead -> Integer -> SymTable -> Maybe [Assembly]
+genComRead :: Generate ComRead
 genComRead (ComRead ids) lev symTable = case ids of
     []          -> Just []
     (id : vs) -> case lookUp symTable id lev of
         Nothing -> Nothing
         Just e  -> (++) <$> Just [Assembly RED (lev - (level e)) (addr e) ""] <*> genComRead (ComRead vs) lev symTable
 
-genComWrite :: ComWrite -> Integer -> SymTable -> Maybe [Assembly]
+genComWrite :: Generate ComWrite
 genComWrite (ComWrite exprs) lev symTable = case exprs of
     []          -> Just []
     (expr : vs) -> (++) <$> ((++) <$> genExpr expr lev symTable <*> Just [Assembly WRT 0 0 ""]) <*> genComWrite (ComWrite vs) lev symTable
 
 
-genSubProgram :: SubProgram -> Integer -> Maybe [Assembly]
-genSubProgram (SubProgram constState varState procedures command) lev = Just $ 
+
+genSubProgram :: Generate SubProgram
+genSubProgram (SubProgram constState varState procedures command) lev symTable = Just $ 
     [Assembly INT 0 (toInteger (length varState)) ""] ++
     [Assembly OPR 0 0 ""]
 
